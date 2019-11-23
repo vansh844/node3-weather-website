74025f1f30c50619e37678faec777f8eb13edfd3
unknown <vansh.100.2020@doonschool.com>
2019-11-23 13:29:53 +0530

    high fixed

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast=(latitude, longitude, callback)=>{
         }else if(body.error){
             callback('Unable to find location')
         }else{
-            callback(undefined,body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degrees out. The high today is '+body.currently.data[0].temperatureHigh+' with a low of '+body.currently.data[0].temperatureLow+'There is '+body.currently.precipProbability+'% chance of rain.')
+            console.log(body.daily.data[0])
+            callback(undefined,body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degrees out. The high today is '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow+'There is '+body.currently.precipProbability+'% chance of rain.')
         }
     })
 }
