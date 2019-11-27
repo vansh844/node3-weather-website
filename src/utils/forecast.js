e40440a1856594ca18302e19a95edf55a6087d8d
unknown <vansh.100.2020@doonschool.com>
2019-11-27 13:26:00 +0530

    Print error

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast=(latitude, longitude, callback)=>{
             callback('Unable to find location')
         }else{
             console.log(body.daily.data[0])
-            callback(undefined,body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degrees out. The high today is '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow+'There is '+body.currently.precipProbability+'% chance of rain.')
+            callback(undefined,body.daily.data[0].summary+ ' It is currently '+body.currently.temperature+' degrees out. The high today is '+body.daily.data[0].temperatureHigh+' with a low of '+body.daily.data[0].temperatureLow+'. There is '+body.currently.precipProbability+'% chance of rain.')
         }
     })
 }
